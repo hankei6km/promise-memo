7c516c3e23db4c6164de93016e480c60470774ab
hankei6km <gh-hankei6km@outlook.jp>
2022-02-27 14:23:36 +0900

    サンプル実行結果を node16 ものに差し替え

--- examples/memo/each_chain_need_catch.ts
+++ examples/memo/each_chain_need_catch.ts
@@ -24,6 +24,6 @@
 // tc1: then catch -> await chain
 // tc2: then -> await chain
 // tc1: rejected
-// (node:2186001) UnhandledPromiseRejectionWarning: rejected
-// (node:2186001) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
-// (node:2186001) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+// [UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "rejected".] {
+//   code: 'ERR_UNHANDLED_REJECTION'
+// }
